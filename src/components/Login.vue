f07d7802f70667c6bdda6aeec6ea32e079a80ce3
Mathew Bramson <mathewbramson@gmail.com>
2017-12-11 21:43:34 -0500

    Add basic login

---
+++ src/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="login">
+    <h1>Feelingsmotron</h1>
+    <input v-model="email" placeholder="email">
+    <input type="password" v-model="password" placeholder="password">
+    <button v-on:click="submitLogin">Submit</button>
+    <h4>{{ status}}</h4>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      status: '',
+    };
+  },
+  methods: {
+    submitLogin: function submitLogin() {
+      this.status = 'Logging in...';
+      axios.post(
+        'http://localhost:4000/api/v1/sessions',
+        { user: {
+          email: this.email,
+          password: this.password,
+        } },
+        )
+        .then((response) => {
+          this.status = `Logged in as ${response.data.user.name}`;
+        })
+        .catch((error) => {
+          if (error.response.status === 401) {
+            this.status = 'Invalid Login Credentials';
+          } else {
+            this.status = `Login failed with error: ${error}`;
+          }
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Feelings from '@/components/Feelings';
+import Login from '@/components/Login';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
     {
       path: '/',
       name: 'Feelings',
