6b86f7d4df5cbdcfc60cc059745fdb40741df38a
Mathew Bramson <mathewbramson@gmail.com>
2018-01-15 21:41:30 -0500

    Add error flash to AppNav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,28 +1,33 @@
 <template>
-  <nav class="navbar navbar-default navbar-expand-lg">
-    <div class="navbar-header">
-      <router-link to="/" class="navbar-brand"> Feelingsmotron </router-link>
-      <button class="navbar-toggle" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="glyphicon glyphicon-th-list"></i>
-      </button>
+  <div class="app-nav">
+    <nav class="navbar navbar-default navbar-expand-lg">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand"> Feelingsmotron </router-link>
+        <button class="navbar-toggle" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="glyphicon glyphicon-th-list"></i>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="nav-item">
+            <router-link to="/groups" class="navbar-link">Groups</router-link>
+          </li>
+          <li class="nav-item" v-if="isLoggedIn">
+            <router-link to="/profile" class="navbar-link">{{ user_name }}</router-link>
+          </li>
+          <li class="nav-item" v-if="isLoggedIn">
+            <a href="#" class="navbar-link" @click="handleLogout()">Log out</a>
+          </li>
+          <li class="nav-item" v-if="!isLoggedIn">
+            <router-link to="/login" class="navbar-link">Log In</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="alert alert-danger">
+      <strong>{{ errorMessage }}</strong>
     </div>
-    <div class="collapse navbar-collapse" id="navbarContent">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item">
-          <router-link to="/groups" class="navbar-link">Groups</router-link>
-        </li>
-        <li class="nav-item" v-if="isLoggedIn">
-          <router-link to="/profile" class="navbar-link">{{ user_name }}</router-link>
-        </li>
-        <li class="nav-item" v-if="isLoggedIn">
-          <a href="#" class="navbar-link" @click="handleLogout()">Log out</a>
-        </li>
-        <li class="nav-item" v-if="!isLoggedIn">
-          <router-link to="/login" class="navbar-link">Log In</router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -32,6 +37,7 @@ export default {
   name: 'app-nav',
   computed: {
     ...mapGetters({
+      errorMessage: 'errorMessage',
       isLoggedIn: 'isLoggedIn',
       user_name: 'user_name',
     }),
