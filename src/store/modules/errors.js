a6ba81f4535da6158f529f1b397e7400513839c2
Mathew Bramson <mathewbramson@gmail.com>
2018-02-03 23:05:45 -0500

    Add field specific error parsing

--- src/store/modules/errors.js
+++ src/store/modules/errors.js
@@ -1,7 +1,19 @@
+import _ from 'lodash';
 import * as types from '../mutation-types';
+
+const fieldSpecificErrorsFromResponse = function fieldSpecificErrorsFromResponse(errorResponse, field) {
+  const data = errorResponse.response.data;
+  return (data.errors || {})[field];
+};
+
+const fieldSpecificErrors = function fieldSpecificErrors(errorResponses, field) {
+  const result = errorResponses.map(e => fieldSpecificErrorsFromResponse(e, field));
+  return _.flatten(result);
+};
 
 const getters = {
   errorResponses: state => state.errorResponses,
+  errorsForNameField: state => fieldSpecificErrors(state.errorResponses, 'name'),
 };
 
 const actions = {
